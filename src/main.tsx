7f80e634dfc552b9ca96c808478c145c618cb235
Liz Bowen <lizbowencoaching@gmail.com>
2025-08-30 16:58:02 -0600

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { Capacitor } from '@capacitor/core';
 import App from './App.tsx';
 import './index.css';
-
-// Initialize Capacitor when running on mobile
-if (Capacitor.isNativePlatform()) {
-  console.log('Running on native platform:', Capacitor.getPlatform());
-}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
